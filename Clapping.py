21a040fd50ddc568a9cc2669a865d7fd8817d01e
Jennifer Smith <43047721+Jenn-L-S@users.noreply.github.com>
2019-10-06 18:44:34 -0400

    Create Clapping.py

---
+++ Clapping.py
@@ -0,0 +1,12 @@
+# Jennifer Smith
+# Code written 09-30-2019.
+
+def main():
+    verbs = ['clap', 'drum', 'tap', 'stamp', 'jump']
+    gerund = ['clapping', 'drumming', 'tapping', 'stamping', 'jumping']
+
+    for k in range(5):
+        print('I traveled over land and sea, \n I met a man who said to me, \n An old, old man who said to me, "Oh where do you come from?" \n "I belong to',
+              gerund[k], 'land,\n To', gerund[k], 'land,', gerund[k], 'land,\n And if you can', verbs[k], 'like me \n Then you can come to', gerund[k], 'land." \n')
+
+main()
